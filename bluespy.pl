a3d8b6c94f7a8fec7e90d66e359f048f197a73af
Eivind Mork <eivindm@gmail.com>
2009-05-06 09:27:43 +0200

    Some more config

--- bluespy.pl
+++ bluespy.pl
@@ -1,11 +1,23 @@
 #! /usr/bin/perl
 #
-#  $Id$
 #
-# Copyright (C) 2009 by Eivind Mork <eivindm@ifi.uio.no>
+#      Copyright (C) 2009 by Eivind Mork <eivindm@ifi.uio.no>
 #
+#
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; version 2 of the License.
+#    
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
 
 
+
+# ubuntu packages:
+#    libqt-perl
+#    libnet-bluetooth-perl
 
 use Net::Bluetooth;
 use strict;
@@ -19,11 +31,11 @@ my $num_checked = 0;
 my $found = {};
 my $name_mapping = {};
 my $found_when = {};
+my $ignore = {};
 
 
 
-
-read_names();
+read_config();
 while (1) {
     scan();
     sleep 5;
@@ -31,21 +43,26 @@ while (1) {
 
 
 
-sub read_names {
+sub read_config {
     open(FIL, $ENV{'HOME'}."/.bluespy") || return;
 
     print "Reading config file\n";
     while (<FIL>) {
         chomp;
-        if (m/^UNIT\s+([A-F0-9:]+)\s+(\w.*)$/) {
+        if (m/^UNIT\s+([A-F0-9:]+)\s+(\w.*)$/i) {
             $name_mapping->{$1} = $2;
         }
 
-        if (m/^FIRSTREPORT\s+(\d+)\s*$/) {
+        if (m/^IGNORE\s+([A-F0-9:]+)/i) {
+            $ignore->{$1} = 1;
+            print "Ignoring address $1\n";
+        }
+
+        if (m/^FIRSTREPORT\s+(\d+)\s*$/i) {
             $report_after = $1;
             print "Will report after $report_after scans\n";
         }
-        if (m/^LEAVELIMIT\s+(\d+)\s*$/) {
+        if (m/^LEAVELIMIT\s+(\d+)\s*$/i) {
             $leavelimit = $1;
             print "Will report leaves after $leavelimit seconds abscense\n";
         }
@@ -64,9 +81,13 @@ sub scan {
         print "Address: $addr Name: $device_ref->{$addr}\n";
         $found_when->{$addr} = time();
         unless ($found->{$addr}) {
-            $found->{$addr} = 1;
-            add_unit($addr, $device_ref->{$addr});
-            push(@found_now, get_name($addr)) unless (get_name($addr) =~ /unknown/i);
+            if ($ignore->{$addr}) {
+                print "Ignoring $addr ($device_ref->{$addr})\n";
+            } else {
+                $found->{$addr} = 1;
+                add_unit($addr, $device_ref->{$addr});
+                push(@found_now, get_name($addr)) unless (get_name($addr) =~ /unknown/i);
+            }
         } else {
             print "\tNo message, found before: $device_ref->{$addr} (".
               get_name($addr).")\n";
@@ -80,21 +101,27 @@ sub scan {
     
     foreach my $addr (keys %$found_when) {
         if (time() - $found_when->{$addr} > $leavelimit) {
-            push(@left_now, get_name($addr)) unless (get_name($addr) =~ /unknown/i);  
+            if ($ignore->{$addr}) {
+                print "Ignoring $addr's leaving ($device_ref->{$addr})\n";
+            } else {
+                push(@left_now, get_name($addr)) unless (get_name($addr) =~ /unknown/i);  
+            }
             delete $found_when->{$addr};
         }
     }
 
     if (scalar @left_now > 0) {        
-        print "We have new devices: \n";
+        print "Someone left: \n";
         $msg .= "Left: ". join(", ",@left_now);
     } 
 
-    if ($msg ne "" && $num_checked >= $report_after) {
-        show_event($msg);
-    } else {
-        print "Not reached reporting limit for message: "
-          .$msg."\n";
+    if ($msg ne "") {
+        if ($num_checked >= $report_after) {
+            show_event($msg);        
+        } else {
+            print "Not reached reporting limit for message: "
+              .$msg."\n";
+        }
     }
 
 
